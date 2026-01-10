6737d16e5e520c8e7d8adfd93a1c4163b183abd5
Edwardz <hanchivi39@gmail.com>
2026-01-10 21:55:10 +1100

    Daily Leetcode Solution (Medium)

---
+++ Stacks/Leetcode_739.py
@@ -0,0 +1,36 @@
+'''
+Given an array of integers temperatures represents the daily temperatures, 
+return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ '''
+
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []
+
+        for i, v in enumerate(temperatures):
+            while stack and stack[-1][1] < v:
+                stack_index, stack_value = stack.pop()
+                ans[stack_index] = i - stack_index
+
+            stack.append((i, v))
+
+        return ans
